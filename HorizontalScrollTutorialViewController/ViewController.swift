cbfaebe5c4343361f0104d3c181776a238a81d9f
大國嗣元 <hideyukitone@mbp-retina-2016.local>
2017-05-13 19:52:12 +0900

    Initial Commit

---
+++ HorizontalScrollTutorialViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HorizontalScrollTutorialViewController
+//
+//  Created by 大國嗣元 on 2017/05/13.
+//  Copyright © 2017年 hideyuki okuni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
