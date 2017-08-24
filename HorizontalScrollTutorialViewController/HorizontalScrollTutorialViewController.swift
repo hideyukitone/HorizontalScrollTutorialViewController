8bb5ef88e67ba216c4272c7fc92c6901b07d6193
hideyukitone <hideyukitone@gmail.com>
2017-08-25 07:10:48 +0900

    使用メモリの軽減

--- HorizontalScrollTutorialViewController/HorizontalScrollTutorialViewController.swift
+++ HorizontalScrollTutorialViewController/HorizontalScrollTutorialViewController.swift
@@ -31,26 +31,32 @@ struct HorizontalScrollTutorialItem {
 
         return (0...CGImageSourceGetCount(cgImageSource)).flatMap{CGImageSourceCreateImageAtIndex(cgImageSource, $0, nil)}.map{UIImage(cgImage: $0)}
     }
+
+    func getImagesInBackground(completion: @escaping ([UIImage]) -> Void) {
+        DispatchQueue.global().async {
+            completion(self.images)
+        }
+    }
 }
 
 class HorizontalScrollTutorialViewController: UIViewController {
     private let scrollView: UIScrollView
     private let bottomLabel: UILabel
     private var viewDidLayoutSubviewsScrollViewWidth: CGFloat?
-    
+
     fileprivate let skipButton: UIButton
     fileprivate let nextButton: UIButton
     fileprivate let pageControl: UIPageControl
     fileprivate let tutorialItems: [HorizontalScrollTutorialItem]
-    
+
     private var titleName: String?
     private var skipButtonName: String
     fileprivate var nextButtonName: String
     fileprivate var doneButtonName: String
     private var isPrefersStatusBarHidden: Bool
-    
+
     var delegate: HorizontalScrollTutorialViewControllerDelegate?
-    
+
     init(tutorialItems: [HorizontalScrollTutorialItem], titleName: String? = nil, skipButtonName: String = "スキップ", nextButtonName: String = "次へ", doneButtonName: String = "閉じる", isPrefersStatusBarHidden: Bool = true) {
         scrollView = UIScrollView(frame: .zero)
         bottomLabel = UILabel(frame: .zero)
@@ -58,19 +64,19 @@ class HorizontalScrollTutorialViewController: UIViewController {
         nextButton = UIButton(frame: .zero)
         pageControl = UIPageControl(frame: .zero)
         self.tutorialItems = tutorialItems
-        
+
         self.titleName = titleName
         self.skipButtonName = skipButtonName
         self.nextButtonName = nextButtonName
         self.doneButtonName = doneButtonName
         self.isPrefersStatusBarHidden = isPrefersStatusBarHidden
-        
+
         super.init(nibName: nil, bundle: nil)
-        
+
         self.modalPresentationStyle = .overCurrentContext
-        
+
         self.view.backgroundColor = UIColor.black.withAlphaComponent(0.7)
-        
+
         bottomLabel.backgroundColor = .black
         self.view.addSubview(bottomLabel)
         bottomLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -78,7 +84,7 @@ class HorizontalScrollTutorialViewController: UIViewController {
         bottomLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
         bottomLabel.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         bottomLabel.heightAnchor.constraint(equalToConstant: 70).isActive = true
-        
+
         skipButton.setTitle(skipButtonName, for: .normal)
         skipButton.setTitleColor(UIColor.lightGray, for: .highlighted)
         skipButton.titleLabel?.setMinimumFontSize(ofSize: 5)
@@ -89,7 +95,7 @@ class HorizontalScrollTutorialViewController: UIViewController {
         skipButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
         skipButton.heightAnchor.constraint(equalTo: bottomLabel.heightAnchor).isActive = true
         skipButton.addTarget(self, action: #selector(self.pressClose), for: .touchUpInside)
-        
+
         nextButton.setTitle(nextButtonName, for: .normal)
         nextButton.setTitleColor(UIColor.lightGray, for: .highlighted)
         nextButton.titleLabel?.setMinimumFontSize(ofSize: 5)
@@ -100,7 +106,7 @@ class HorizontalScrollTutorialViewController: UIViewController {
         nextButton.widthAnchor.constraint(equalTo: skipButton.widthAnchor).isActive = true
         nextButton.heightAnchor.constraint(equalTo: skipButton.heightAnchor).isActive = true
         nextButton.addTarget(self, action: #selector(self.pressNext), for: .touchUpInside)
-        
+
         pageControl.numberOfPages = tutorialItems.count
         self.view.addSubview(pageControl)
         pageControl.translatesAutoresizingMaskIntoConstraints = false
@@ -110,7 +116,7 @@ class HorizontalScrollTutorialViewController: UIViewController {
         pageControl.heightAnchor.constraint(equalTo: skipButton.heightAnchor).isActive = true
         pageControl.addTarget(self, action: #selector(self.pressPageControl), for: .touchUpInside)
         pageControl.addTarget(self, action: #selector(self.changePageControl), for: UIControlEvents.valueChanged)
-        
+
         var topLabel: UILabel? = nil
         if let titleName = titleName {
             topLabel = UILabel(frame: .zero)
@@ -129,7 +135,7 @@ class HorizontalScrollTutorialViewController: UIViewController {
                 topLabel.heightAnchor.constraint(equalTo: bottomLabel.heightAnchor).isActive = true
             }
         }
-        
+
         scrollView.delegate = self
         scrollView.isScrollEnabled = true
         scrollView.isPagingEnabled = true
@@ -142,68 +148,77 @@ class HorizontalScrollTutorialViewController: UIViewController {
         scrollView.topAnchor.constraint(equalTo: topLabel?.bottomAnchor ?? self.topLayoutGuide.bottomAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: skipButton.topAnchor).isActive = true
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
     }
-    
+
+    private struct TutorialImageInformation {
+        let first: UIImage?
+        let last: UIImage?
+        let count: Int
+    }
+
+    private var tutorialImageInformations = [TutorialImageInformation]()
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+
         if let viewDidLayoutSubviewsScrollViewWidth = viewDidLayoutSubviewsScrollViewWidth
             , viewDidLayoutSubviewsScrollViewWidth == scrollView.frame.width {
             return
         }
-        
+
         scrollView.subviews.flatMap{$0 as? UIImageView}.forEach{$0.stopAnimating() ; $0.removeFromSuperview()}
 
         for (i, tutorialItem) in tutorialItems.enumerated() {
-            let images = tutorialItem.images
+            var images = tutorialItem.images
+            let tutorialImageInformation = TutorialImageInformation(first: images.first, last: images.last, count: images.count)
             let imageView = UIImageView(image: images.first)
+            images = []
+            tutorialImageInformations.append(tutorialImageInformation)
 
+            imageView.image = tutorialImageInformation.first
             imageView.frame.size = scrollView.frame.size
             imageView.frame.origin.y = 0
             imageView.frame.origin.x = CGFloat(i) * scrollView.frame.size.width
             imageView.contentMode = .scaleAspectFit
             imageView.tag = i
 
-            if images.count > 1 {
-                imageView.animationImages = images
-                imageView.animationDuration = 3
-                imageView.animationRepeatCount = 1
-            }
+            imageView.animationDuration = 3
+            imageView.animationRepeatCount = 1
 
             scrollView.addSubview(imageView)
         }
-        
+
         scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(tutorialItems.count), height: scrollView.frame.size.height)
-        
+
         movePage(page: 0, animated: false)
-        
+
         viewDidLayoutSubviewsScrollViewWidth = scrollView.frame.width
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     override var prefersStatusBarHidden: Bool {
         return isPrefersStatusBarHidden
     }
-    
+
     func pressClose() {
         self.dismiss(animated: true) {
             self.delegate?.horizontalScrollTutorialViewControllerDidFinish()
         }
     }
-    
+
     func pressNext() {
         if skipButton.isHidden {
             pressClose()
@@ -211,20 +226,58 @@ class HorizontalScrollTutorialViewController: UIViewController {
             movePage(page: pageControl.currentPage + 1, animated: true)
         }
     }
-    
+
     func pressPageControl() {
         movePage(page: pageControl.currentPage, animated: false)
     }
-    
-    func changePageControl() {
-        scrollView.subviews.flatMap{$0 as? UIImageView}.filter{$0.tag == pageControl.currentPage && $0.animationImages?.count ?? 0 > 1}.forEach{$0.stopAnimating() ; $0.image = $0.animationImages?.last ; $0.startAnimating()}
 
-        scrollView.subviews.flatMap{$0 as? UIImageView}.filter{$0.tag != pageControl.currentPage && $0.animationImages?.count ?? 0 > 1}.forEach{$0.stopAnimating() ; $0.image = $0.animationImages?.first}
+    private var previousPage: Int?
+
+    func changePageControl() {
+        guard previousPage != pageControl.currentPage else {
+            return
+        }
+
+        func startImageView(imageView: UIImageView) {
+            guard tutorialImageInformations[imageView.tag].count > 1 else {
+                return
+            }
+
+            tutorialItems[imageView.tag].getImagesInBackground { (images) in
+                imageView.animationImages = images
+
+                DispatchQueue.main.async() { () -> Void in
+                    guard imageView.tag == self.pageControl.currentPage else {
+                        return
+                    }
+
+                    imageView.image = self.tutorialImageInformations[imageView.tag].last
+                    imageView.startAnimating()
+                }
+            }
+        }
+
+        func endImageView(imageView: UIImageView) {
+            imageView.stopAnimating()
+            imageView.animationImages = []
+
+            if let previousPage = previousPage, previousPage == imageView.tag {
+                imageView.image = tutorialImageInformations[imageView.tag].last
+            }else {
+                imageView.image = tutorialImageInformations[imageView.tag].first
+            }
+        }
+
+        scrollView.subviews.flatMap{$0 as? UIImageView}.forEach{endImageView(imageView: $0)}
+
+        scrollView.subviews.flatMap{$0 as? UIImageView}.filter{$0.tag == pageControl.currentPage}.forEach{startImageView(imageView: $0)}
+
+        previousPage = pageControl.currentPage
     }
-    
+
     private func movePage(page: Int, animated: Bool) {
         scrollView.setContentOffset(CGPoint(x: CGFloat(page) * scrollView.frame.size.width, y: 0), animated: animated)
-        
+
         if !animated {
             changePageControl()
         }
@@ -237,10 +290,10 @@ extension HorizontalScrollTutorialViewController: UIScrollViewDelegate {
             pageControl.currentPage = 0
             return
         }
-        
+
         pageControl.currentPage = Int(scrollView.contentOffset.x / scrollView.frame.width)
         skipButton.isHidden = pageControl.currentPage == tutorialItems.count - 1
-        
+
         if skipButton.isHidden {
             nextButton.setTitle(doneButtonName, for: .normal)
         }else {
